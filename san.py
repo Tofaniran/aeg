250521bee267680ec8b3b6faada51262ba16beef
TOFAN <141255314+Tofaniran@users.noreply.github.com>
2023-12-17 03:07:14 +0330

    pida karden san
    san shomar

---
+++ san.py
@@ -0,0 +1,22 @@
+from colorama import Fore
+
+for x in range(4):
+    for y in range(10):
+        print(Fore.BLUE + "(/$$$TOFAN$$$\ {x}", end= '')
+print()
+
+name = input("wat is your name: ")
+
+print(" hello " + name + " welcam san yor ")
+
+mohasbe = input("tarikh tavalod: ")
+for cal in range(1300,1403):
+     print(cal)
+age = 1402 - int(mohasbe)
+
+print("__"*30)
+print(age)
+print(name)
+print(mohasbe)
+
+print(Fore.GREEN + "ok san yor")
